dad2eb67d136d9dbfd945fa7bab6af0ba44a57ef
Daniel P <mail@danielplatt.de>
2023-08-28 00:14:16 +0200

    fix phpstan errors

--- src/GitDevInsights/CodeInsights/Results/FileExtensionAnalysisResult.php
+++ src/GitDevInsights/CodeInsights/Results/FileExtensionAnalysisResult.php
@@ -8,11 +8,17 @@ class FileExtensionAnalysisResult
      */
     private array $data;
 
+    /**
+     * @param array<string, int> $data
+     */
     public function __construct(array $data = [])
     {
         $this->data = $data;
     }
 
+    /**
+     * @return array<string, int>
+     */
     public function getData(): array
     {
         return $this->data;
