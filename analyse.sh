1bdf7fe8b7cb17f372b04ea73074ff9c87faa08d
Daniel P <mail@danielplatt.de>
2023-08-28 20:28:58 +0200

    new usage

--- analyse.sh
+++ analyse.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-php analyse.php --config project-configs/phpmyadmin.yaml
+php analyse.php --config project-configs/phpmyadmin.yaml --outputPath source-repo/generated-stats
+# next: analys in Schleife mit Checkout und switch Monat für Monat rückwärts
