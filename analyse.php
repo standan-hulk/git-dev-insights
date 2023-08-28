66ee25d4a6091e252de22673dab3e71614b64527
Daniel P <mail@danielplatt.de>
2023-08-28 23:28:02 +0200

    almost working state

--- analyse.php
+++ analyse.php
@@ -17,19 +17,38 @@ if (isset($argv[1]) && $argv[1] === '--config') {
 
     $projectConfigDataProvider = new ProjectConfigDataProvider($projectConfigFile);
     $analysisResult = new AnalysisResult();
+
+    // checkout the repo
+    $command = 'git clone '.$projectConfigDataProvider->repositoryUrl;
+    shell_exec($command);
+
+    $tsChartTime = strtotime('last Monday');
+    $targetDate = date('Y-m-d', $tsChartTime);
+
+    $weekInSeconds = 7 * 24 * 60 * 60; // One week in seconds
+
     $codeInsightsService = new CodeInsightsService($projectConfigDataProvider, $analysisResult);
-    $codeInsightsService->analyse(time());
 
-  //  $analysisResult->outputToJsonFile($jsonOutputPath);
+    for($i = 0; $i < 5; $i++) {
+        $codeInsightsService->analyse($tsChartTime);
+
+        $commitHash = shell_exec("cd ".$projectConfigDataProvider->checkoutPath. " && git rev-list -n 1 --before='". $targetDate ."' HEAD");
+
+        $output = shell_exec("cd ".$projectConfigDataProvider->checkoutPath ." && git checkout ".$commitHash);
+
+        $tsChartTime = $tsChartTime - $weekInSeconds;
+        $targetDate = date('Y-m-d', $tsChartTime);
+        dump($targetDate);
+    }
+
+
     $jsonData = $analysisResult->__toJsonData();
-
     $htmlOutput = new LanguageChartHTMLFileGenerator($jsonData);
 
     $html = $htmlOutput->renderChartOutput();
 
     $fileName = $projectConfigDataProvider->analyseResultPath.'/chart.html';
     $result = $htmlOutput->writeChartOutputToFile($fileName);
-
 
 } else {
     echo "Usage: php analyse.php --config [project config file] [--outputPath [path of the generated insights]]\n";
